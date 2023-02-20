69a2ea78f921dad939fe0dba48cc177957dde210
daniil0040 <danyapolishchuk777@gmail.com>
2023-02-20 10:53:01 +0200

    hw7

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,14 @@
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
-  function toggleModal() {
+  function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
